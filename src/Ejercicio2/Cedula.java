c101cb15620bee37506872cafe7d13dd0958310f
Thiago Sosa <sosat279@gmail.com>
2025-08-15 22:07:52 -0300

    Agregamos cambios en tiempo real

--- src/Ejercicio2/Cedula.java
+++ src/Ejercicio2/Cedula.java
@@ -1,8 +1,11 @@
 package Ejercicio2;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Cedula extends JFrame {
     private JPanel ventana;
@@ -17,46 +20,7 @@ public class Cedula extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String cedula = txtCedula.getText().trim();
-                boolean cedulaValido = false;
-
-                if (cedula.length() == 8 && cedula.matches("\\d+")) {
-                    // Separamos los dígitos
-                    String dig1 = cedula.substring(0, 1);
-                    String dig2 = cedula.substring(1, 2);
-                    String dig3 = cedula.substring(2, 3);
-                    String dig4 = cedula.substring(3, 4);
-                    String dig5 = cedula.substring(4, 5);
-                    String dig6 = cedula.substring(5, 6);
-                    String dig7 = cedula.substring(6, 7);
-                    String dig8 = cedula.substring(7, 8);
-
-                    // Convertimos los String a Int
-                    int dig1Convrt = Integer.parseInt(dig1);
-                    int dig2Convrt = Integer.parseInt(dig2);
-                    int dig3Convrt = Integer.parseInt(dig3);
-                    int dig4Convrt = Integer.parseInt(dig4);
-                    int dig5Convrt = Integer.parseInt(dig5);
-                    int dig6Convrt = Integer.parseInt(dig6);
-                    int dig7Convrt = Integer.parseInt(dig7);
-                    int dig8Convrt = Integer.parseInt(dig8);
-
-                    // Multiplicar y sumar los dígitos del 1 al 7
-                    int sumaMulti = dig1Convrt * 2 + dig2Convrt * 9 + dig3Convrt * 8 +
-                            dig4Convrt * 7 + dig5Convrt * 6 + dig6Convrt * 3 + dig7Convrt * 4;
-
-                    // Calculo módulo de sumaMulti
-                    int moduloSumaMulti = sumaMulti % 10;
-
-                    // Ajuste en el cálculo de la resta
-                    int resultadoRestaSumaMulti = (moduloSumaMulti == 0) ? 0 : 10 - moduloSumaMulti;
-
-                    // Controlamos si el dig8 coincide con nuestro dígito calculado
-                    if (dig8Convrt == resultadoRestaSumaMulti) {
-                        cedulaValido = true;
-                    } else {
-                        cedulaValido = false;
-                    }
-                }
+                boolean cedulaValido = validarCedula(cedula);
 
                 // Mostrar mensaje
                 if (cedulaValido) {
@@ -66,8 +30,70 @@ public class Cedula extends JFrame {
                 }
             }
         });
-    }
 
+        txtCedula.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                if(txtCedula.getText().length() == 0){
+                    txtCedula.setBackground(Color.red);
+                }else if(txtCedula.getText().length() > 8){
+                    txtCedula.setBackground(Color.yellow);
+                }else if(validarCedula(txtCedula.getText())){
+                    txtCedula.setBackground(Color.green);
+                }else if(!validarCedula(txtCedula.getText())){
+                    txtCedula.setBackground(Color.red);
+                }
+                else{
+                    txtCedula.setBackground(Color.white);
+                }
+            }
+        });
+    }
+    private boolean validarCedula(String cedula) {
+        boolean cedulaValido = false;
+
+        if (cedula.length() == 8 && cedula.matches("\\d+")) {
+            // Separamos los dígitos
+            String dig1 = cedula.substring(0, 1);
+            String dig2 = cedula.substring(1, 2);
+            String dig3 = cedula.substring(2, 3);
+            String dig4 = cedula.substring(3, 4);
+            String dig5 = cedula.substring(4, 5);
+            String dig6 = cedula.substring(5, 6);
+            String dig7 = cedula.substring(6, 7);
+            String dig8 = cedula.substring(7, 8);
+
+            // Convertimos los String a Int
+            int dig1Convrt = Integer.parseInt(dig1);
+            int dig2Convrt = Integer.parseInt(dig2);
+            int dig3Convrt = Integer.parseInt(dig3);
+            int dig4Convrt = Integer.parseInt(dig4);
+            int dig5Convrt = Integer.parseInt(dig5);
+            int dig6Convrt = Integer.parseInt(dig6);
+            int dig7Convrt = Integer.parseInt(dig7);
+            int dig8Convrt = Integer.parseInt(dig8);
+
+            // Multiplicar y sumar los dígitos del 1 al 7
+            int sumaMulti = dig1Convrt * 2 + dig2Convrt * 9 + dig3Convrt * 8 +
+                    dig4Convrt * 7 + dig5Convrt * 6 + dig6Convrt * 3 + dig7Convrt * 4;
+
+            // Calculo módulo de sumaMulti
+            int moduloSumaMulti = sumaMulti % 10;
+
+            // Ajuste en el cálculo de la resta
+            int resultadoRestaSumaMulti = (moduloSumaMulti == 0) ? 0 : 10 - moduloSumaMulti;
+
+            // Controlamos si el dig8 coincide con nuestro dígito calculado
+            if (dig8Convrt == resultadoRestaSumaMulti) {
+                cedulaValido = true;
+            } else {
+                cedulaValido = false;
+            }
+        }
+
+        return cedulaValido;
+    }
     public static void main(String[] args) {
         Cedula ventanaCedula = new Cedula();
         ventanaCedula.setContentPane(ventanaCedula.ventana);
